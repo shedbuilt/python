9c2b2dbc251abffde83159d92eb94b7d948377f7
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:27:42 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@
             --with-system-ffi \
             --with-ensurepip=yes \
             --enable-unicode=ucs4
-make -j $SHED_NUMJOBS
-make DESTDIR=${SHED_FAKEROOT} install
-chmod -v 755 ${SHED_FAKEROOT}/usr/lib/libpython2.7.so.1.0
+make -j $SHED_NUM_JOBS
+make DESTDIR=${SHED_FAKE_ROOT} install
+chmod -v 755 ${SHED_FAKE_ROOT}/usr/lib/libpython2.7.so.1.0
